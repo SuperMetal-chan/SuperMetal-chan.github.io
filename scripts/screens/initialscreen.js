86e902fd68f659f84e986d759ec3f303fdd59178
Artyom Procenko <supermetalkiller@gmail.com>
2022-11-09 21:40:01 +0200

    Fixes

--- scripts/screens/initialscreen.js
+++ scripts/screens/initialscreen.js
@@ -7,24 +7,16 @@ function InitialScreen(isActive) {
 InitialScreen.prototype.start = function(){
 	
 	this.scene.setFunctionStart(function () {
-
+		new Rect(0,0, canvas.height, canvas.width, "#101118");
+		// var tx = new Text("Apoio", 430, 250);
 		// var anim =  new Animation();
-		// //anim.insertRepeatAnimation( "logo_fapesb", 10  );
+		// anim.insertRepeatAnimation( "logo_unifacs", 10  );
 		// anim.insertAnimation( "fadein", null, 10 );
 		// anim.setLoop(false);
-		//
-		// var sp2 = new Sprite( [anim], 700, 275, 120, 65);
-
-		new Rect(0,0, canvas.height, canvas.width, "#101118");
-		var tx = new Text("Apoio", 430, 250);
-		var anim =  new Animation();
-		anim.insertRepeatAnimation( "logo_unifacs", 10  );
-		anim.insertAnimation( "fadein", null, 10 );
-		anim.setLoop(false);
 
 		var sp = new Sprite( [anim] , 380, 300, 150, 30);
 
-		var tx2 = new Text("Financiamento", 690, 250)
+		// var tx2 = new Text("Financiamento", 690, 250)
 
 		var anim =  new Animation();
 		anim.insertRepeatAnimation( "logo_fapesb", 10  );
@@ -46,7 +38,7 @@ InitialScreen.prototype.start = function(){
 			
 			var sp3 = new Sprite( [anim] , 270, 250, 640, 88);
 			
-		}, 3000);
+		}, 2000);
 		
 		setTimeout( 
 		function(){  
